cb38d07ddedd7194337e07b55cfddaec9d359255
Abhishek <amishra18101997@gmail.com>
2024-02-26 16:38:46 +0530

    Blog_API_app

---
+++ src/main/java/com/blogapp/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.blogapp.repositories;
+
+import java.util.Optional;
+
+import com.blogapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+		
+	
+	Optional<User> findByEmail(String email);
+}
